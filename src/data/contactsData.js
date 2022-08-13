6551fa40ad9c54800dbc68ac230b54b859956440
abrehamhub <abrehamgebremedin12@gmail.com>
2022-08-13 10:44:05 +0300

    initial commit

---
+++ src/data/contactsData.js
@@ -0,0 +1,8 @@
+export const contactsData = {
+    email: 'abreham.gebremedin12@gmail.com,  abreham.gebremedin@uog.edu.et',
+    phone: '+251919176988,  +251970493518',
+    address: 'Addis Ababa, Ethiopia ',
+    
+
+    sheetAPI: ''
+}
---
+++ src/data/educationData.js
@@ -0,0 +1,16 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'Addis Ababa University',
+        course: 'Bachelor of Science in Information SYstems',
+        startYear: 'September, 2014',
+        endYear: 'July, 2018'
+    },
+    {
+        id: 2,
+        institution: 'University of Gondar',
+        course: 'Master of Science in Information SYstems ',
+        startYear: 'September, 2020',
+        endYear: 'July, 2022'
+    },
+]
