e1b451b20ab526c5ffcbcaffd45960199dac6e1a
abrehamhub <abrehamgebremedin12@gmail.com>
2022-08-13 16:41:49 +0300

    update

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,6 +1,6 @@
-import React,{useContext} from 'react';
-
+import React from 'react';
 import { ThemeContext } from '../../contexts/ThemeContext';
+import {useContext} from 'react';
 
 import { servicesData } from '../../data/servicesData';
 
